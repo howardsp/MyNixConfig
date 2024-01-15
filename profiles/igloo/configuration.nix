978e62e481d9d3c31055693db9e0ad3934f48ca2
Howard Spector <howardsp@gmail.com>
2024-01-14 19:55:00 -0500

    config updates

--- profiles/igloo/configuration.nix
+++ profiles/igloo/configuration.nix
@@ -10,15 +10,17 @@
   networking.hostName = "igloo"; # Define your hostname.
 
   # Kernel modules  
-  boot.kernelModules = [ "kvm-amd" "kvm-intel" "cpufreq_performance" ];    
+  boot.kernelModules = [ "kvm-amd" "cpufreq_performance" ];    
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_6;
 
   virtualisation.libvirtd.enable = true;
 
   services.synergy.server.enable = true;
   networking.firewall.allowedTCPPortRanges = [ { from = 24800; to = 24801; } ];
   networking.firewall.allowedUDPPortRanges = [ { from = 24800; to = 24801; } ];
+
 
 
 environment.systemPackages = with pkgs; [
