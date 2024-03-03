6f742c8ac64a7e574c6322efd527d7b4a31def56
Howard Spector <howardsp@gmail.com>
2024-03-02 20:14:48 -0500

    fixing citrix

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
       nixosConfigurations = { 
         flakebook = nixpkgs.lib.nixosSystem {             
         inherit system;
-        specialArgs = { inherit pkgs-stable; };
+        extraSpecialArgs = { inherit pkgs-stable; };
         modules = [
             (./profiles/flakebook/configuration.nix)
             home-manager.nixosModules.home-manager {
