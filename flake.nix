986cb8fb09789bf3e183d588e68b76e51cac44ab
Howard Spector <howardsp@gmail.com>
2024-11-26 21:52:11 -0500

    adding mac

--- flake.nix
+++ flake.nix
@@ -16,13 +16,63 @@
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";            
 
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";    
+    homebrew-bundle.url = "github:homebrew/homebrew-bundle";
+    homebrew-bundle.flake = false;    
+    homebrew-core.url = "github:homebrew/homebrew-core";
+    homebrew-core.flake = false;    
+    homebrew-cask.url = "github:homebrew/homebrew-cask";
+    homebrew-cask.flake = false;
     #exfoliate.url = "github:howardsp/exfoliate";    
   };
     
-  outputs = { self, nixpkgs, nixpkgs-stable, home-manager,  ... } @inputs:
+  outputs = { self, nixpkgs, nixpkgs-stable, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager,  ... } @inputs:
     
     let       
       allowUnfree = { nixpkgs.config.allowUnfree = true; };          
+
+      createSystemMAC = { host, username ? "howardsp", fullname ? "Howard Spector", system ? "x86_64-linux"  }: darwin.lib.darwinSystem {            
+          modules = [
+            (./hosts/${host}.nix)            
+            (allowUnfree)
+            home-manager.nixosModules.home-manager {
+              home-manager.useUserPackages = true;
+              home-manager.useGlobalPkgs = true;
+              home-manager.users.${username} = (./users/${username}-${host}.nix);
+              home-manager.extraSpecialArgs = {                
+                pkgs-stable = import nixpkgs-stable  {
+                    inherit system;
+                    config.allowUnfree = true;
+                };             
+                inherit host username fullname;
+                };     
+            } 
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                inherit username;
+                enable = true;
+                taps = {
+                  "homebrew/homebrew-core" = homebrew-core;
+                  "homebrew/homebrew-cask" = homebrew-cask;
+                  "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+                mutableTaps = false;
+                autoMigrate = true;
+              };
+            }            
+          ];
+          specialArgs = { 
+            pkgs-stable = import nixpkgs-stable  {
+              inherit system;
+              config.allowUnfree = true;
+            };             
+          inherit  host username fullname  inputs; };
+        };
+                  
+
       createSystem = { host, username ? "howardsp", fullname ? "Howard Spector", system ? "x86_64-linux"  }: nixpkgs.lib.nixosSystem {                
         inherit system;     
         
@@ -54,12 +104,16 @@
 
     in {               
 
-      nixosConfigurations = {            
-          #igloo = exfoliate.lib.createSystem { host="igloo"; };
+      nixosConfigurations = {                     
           igloo = createSystem { host="igloo"; };
           flakebook = createSystem {host = "flakebook";};
           virtualnix = createSystem {host = "virtualnix";};
           avalanche = createSystem {host = "avalanche";};
+          flakymac = createSystem {host = "flakymac"; system = "x86_64-darwin"; };
       };
-  };      
-} 
+
+      darwinConfigurations = {
+        flakymac = createSystemMAC {host = "flakymac"; system = "x86_64-darwin"; };        
+      };        
+    };
+}
---
+++ hosts/flakymac.nix
@@ -0,0 +1,66 @@
+{ config, lib, pkgs, host, ... }:
+{
+ #imports = [ ../packages/system ];
+
+  homebrew = {
+    # This is a module from nix-darwin
+    # Homebrew is *installed* via the flake input nix-homebrew
+    enable = true;
+
+  environment.systemPackages = with pkgs; [
+    firefox         
+    fastfetch
+    mas
+    git  
+    vim
+    neovim
+    wget
+    curl
+    zip
+    unzip
+    htop         
+    tldr         # short version of man
+    bat          # better cat   
+    nvd          # nixs version diff
+    nh           # nixs helper
+    duf du-dust  # replacements for du and df
+    fd           # fdfind is an enhanced find
+    ripgrep      # rga will seach in all kinds of files.
+    choose       # (the basics of awk/cut)
+    direnv       # load environment variables depending on the current directory
+    entr         # run arbitrary commands when files chaneg
+    sd           # sed alternative
+    difftastic   # diff that understands code
+    httpie       # command line http client
+    curlie       # frontend to curl adds ease of use
+    miller       # like sed, awk, ... for formated files csv, json..
+    gtop         # command line visual top
+    fzf          # fuzzy find for the command line
+  ];
+  
+    brews = [
+
+    ];
+    casks = [
+      # Communication Tools
+      "discord"    
+      "telegram"
+      "zoom"
+      "citrix"
+      "google-chrome"
+      "firefox"
+      "microsoft-office"
+    ];
+
+    # These app IDs are from using the mas CLI app
+    # mas = mac app store https://github.com/mas-cli/mas
+    #$ nix shell nixpkgs#mas
+    #$ mas search <app name>    
+    masApps = {
+      "1password" = 1333542190;
+      "hidden-bar" = 1452453066;
+      "wireguard" = 1451685025;
+    };
+  };
+ 
+}
