007337579220bd44dc018c043928040533376521
Howard Spector <howardsp@gmail.com>
2024-11-27 20:20:48 -0500

    fix

--- users/howardspector-Howards-MacBook-Pro.nix
+++ users/howardspector-Howards-MacBook-Pro.nix
@@ -2,7 +2,7 @@
 
 {
     home.username = username;
-    home.homeDirectory = "/Users/${username}" ;
+    home.homeDirectory = /Users/${username};
     home.stateVersion = "24.11"; 
     programs.home-manager.enable = true;
 
