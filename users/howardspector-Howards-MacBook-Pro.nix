c2275662a0ed2aa4c063bdb6c23bea17a457ec65
Howard Spector <howardsp@gmail.com>
2024-11-27 20:18:44 -0500

    fix

--- users/howardspector-Howards-MacBook-Pro.nix
+++ users/howardspector-Howards-MacBook-Pro.nix
@@ -6,32 +6,4 @@
     home.stateVersion = "24.11"; 
     programs.home-manager.enable = true;
 
-    homebrew = {
-      # This is a module from nix-darwin
-      # Homebrew is *installed* via the flake input nix-homebrew
-      enable = true;
-
-      #brews = [ ];
-
-      casks = [
-        # Communication Tools
-        "discord"    
-        "telegram"
-        "zoom"
-        "citrix"
-        "google-chrome"
-        "firefox"
-        "microsoft-office"
-      ];
-
-      # These app IDs are from using the mas CLI app
-      # mas = mac app store https://github.com/mas-cli/mas
-      #$ nix shell nixpkgs#mas
-      #$ mas search <app name>    
-      #masApps = {
-      #  #"1password" = 1333542190;
-      #  "hidden-bar" = 1452453066;
-      #  #"wireguard" = 1451685025;
-      #};
-  };
 }
