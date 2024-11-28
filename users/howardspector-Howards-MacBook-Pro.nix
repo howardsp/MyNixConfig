7699f2dcb643da2d99f03db7bf74eaf3ee36dc53
Howard Spector <howardsp@gmail.com>
2024-11-27 20:21:43 -0500

    fix

--- users/howardspector-Howards-MacBook-Pro.nix
+++ users/howardspector-Howards-MacBook-Pro.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, username,  ... }:
 
-{
-    home.username = username;
-    home.homeDirectory = /Users/${username};
+{   
     home.stateVersion = "24.11"; 
     programs.home-manager.enable = true;
 
