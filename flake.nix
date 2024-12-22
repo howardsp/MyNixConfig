3500e97be0e7b68224374d2b396f8833b5f78aa4
Howard Spector <howardsp@gmail.com>
2024-12-22 15:55:33 -0500

    function refactor

--- flake.nix
+++ flake.nix
@@ -9,14 +9,11 @@
 # nix run nix-darwin -- switch --flake 
 {
   description = "Howard's NIXOS Configuraiton that supports multiple machines";
-  inputs = 
-  {
+  inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";    
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
-  
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";            
-
     darwin.url = "github:LnL7/nix-darwin/master";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";    
@@ -25,96 +22,26 @@
     homebrew-core.url = "github:homebrew/homebrew-core";
     homebrew-core.flake = false;    
     homebrew-cask.url = "github:homebrew/homebrew-cask";
-    homebrew-cask.flake = false;
+    homebrew-cask.flake = false;    
   };
     
-  outputs = { self, nixpkgs, nixpkgs-stable, darwin, nix-homebrew, homebrew-bundle, homebrew-core, homebrew-cask, home-manager,  ... } @inputs:
-    
+  outputs = { self, nixpkgs, nixpkgs-stable, darwin, nix-homebrew, homebrew-bundle, 
+              homebrew-core, homebrew-cask, home-manager, ... } @inputs:    
     let       
-      allowUnfree = { nixpkgs.config.allowUnfree = true; };               
 
-      createSystemDarwin = { host, username ? "howardsp", fullname ? "Howard Spector", thesystem ? "aarch64-darwin"  }: darwin.lib.darwinSystem {            
-          system = thesystem;
-          
-          modules = [
-            (./hosts/${host}.nix)            
-            (allowUnfree)
-            home-manager.darwinModules.home-manager {
-              home-manager.useUserPackages = true;
-              home-manager.useGlobalPkgs = true;
-              home-manager.users.${username} = (./users/${username}-${host}.nix);                                       
-              home-manager.extraSpecialArgs = {                
-              pkgs-stable = import nixpkgs-stable  {
-                    system = thesystem;
-                    config.allowUnfree = true;
-                };             
-                inherit host username fullname thesystem;
-                };     
-            } 
-            nix-homebrew.darwinModules.nix-homebrew
-            {
-              nix-homebrew = {                
-                enable = true;
-                #enableRosetta = true;
-                user = "${username}";
-                taps = {
-                  "homebrew/homebrew-core" = homebrew-core;
-                  "homebrew/homebrew-cask" = homebrew-cask;
-                  "homebrew/homebrew-bundle" = homebrew-bundle;
-                };
-                mutableTaps = false;
-                autoMigrate = true;
-              };
-            }            
-          ];
-          specialArgs = { 
-            pkgs-stable = import nixpkgs-stable  {
-              system = thesystem;
-              config.allowUnfree = true;
-            };             
-          inherit  host username fullname  inputs thesystem; };
+      helpers = import ./flakehelper.nix {inherit nixpkgs nixpkgs-stable home-manager nix-homebrew 
+                                                  homebrew-core homebrew-cask homebrew-bundle;};
+
+      in {               
+
+        nixosConfigurations = {                     
+          igloo = helpers.createSystem {helpers = helpers; host = "igloo";};
+          virtualnix = helpers.createSystem {helpers = helpers; host = "virtualnix";};
+          avalanche = helpers.createSystem {helpers = helpers; host = "avalanche";};          
         };
-                  
 
-      createSystem = { host, username ? "howardsp", fullname ? "Howard Spector", thesystem ? "x86_64-linux"  }: nixpkgs.lib.nixosSystem {
-        system = thesystem;
-        
-        modules = [            
-            (./hosts/${host}.nix)
-            (./hardware/hardware-${host}.nix)            
-            (allowUnfree)
-            home-manager.nixosModules.home-manager {
-              home-manager.useUserPackages = true;
-              home-manager.useGlobalPkgs = true;
-              home-manager.users.${username} = (./users/${username}-${host}.nix);
-              home-manager.extraSpecialArgs = {                
-                pkgs-stable = import nixpkgs-stable  {
-                    system = thesystem;
-                    config.allowUnfree = true;
-                };             
-
-                inherit host username fullname thesystem;
-                };     
-            } 
-          ]; 
-          specialArgs = { 
-              pkgs-stable = import nixpkgs-stable  {
-                  system = thesystem;
-                  config.allowUnfree = true;
-              };             
-              inherit  host username fullname  home-manager thesystem;};
-        }; 
-
-    in {               
-
-      nixosConfigurations = {                     
-          igloo = createSystem { host="igloo"; };          
-          virtualnix = createSystem {host = "virtualnix";};
-          avalanche = createSystem {host = "avalanche";};          
-      };
-
-      darwinConfigurations = {
-        NixBookAir = createSystemDarwin {username = "howardsp"; host = "nixbookair";  };        
-      };        
+        darwinConfigurations = {
+          nixbookair = helpers.createSystemDarwin {helpers = helpers; host = "nixbookair";  };        
+        };        
     };
 }
--- flakehelper.nix
+++ flakehelper.nix
@@ -1,62 +1,67 @@
-# system build functions 
+{ nixpkgs, nixpkgs-stable,home-manager, nix-homebrew, homebrew-core, homebrew-cask, homebrew-bundle, ... }: {                    
 
-#host, username ? "howardsp", fullname ? "Howard Spector", thesystem ? "x86_64-linux"  : nixpkgs.lib.nixosSystem {
-let
-
-  createLinuxSystemModules = host: username: fullname: thesystem: rec {
-    modules = [            
-        (./hosts/${host}.nix)
-        (./hardware/hardware-${host}.nix)            
-        (allowUnfree)
-        home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.useGlobalPkgs = true;
-            home-manager.users.${username} = (./users/${username}-${host}.nix);
-            home-manager.extraSpecialArgs = {                
+    allowUnfree = { nixpkgs.config.allowUnfree = true; };       
+    
+    homeHelper = {username ? "howardsp", fullname, host, thesystem }: {
+        home-manager.useUserPackages = true;
+        home-manager.useGlobalPkgs = true;
+        home-manager.users.${username} = (./users/${username}-${host}.nix);
+        home-manager.extraSpecialArgs = {                
             pkgs-stable = import nixpkgs-stable  {
                 system = thesystem;
                 config.allowUnfree = true;
             };             
-
             inherit host username fullname thesystem;
-            };     
-        } 
-        ]; 
-        specialArgs = { 
-            pkgs-stable = import nixpkgs-stable  {
-                system = thesystem;
-                config.allowUnfree = true;
-            };             
-            inherit  host username fullname  home-manager thesystem;};
+        };                     
+    }; 
+
+    homeBrewHelper = {username ? "howardsp" }: nix-homebrew.darwinModules.nix-homebrew {
+        nix-homebrew = {                
+            enable = true;
+            #enableRosetta = true;
+            user = "${username}";
+            taps = {
+                "homebrew/homebrew-core" = homebrew-core;
+                "homebrew/homebrew-cask" = homebrew-cask;
+                "homebrew/homebrew-bundle" = homebrew-bundle;
+                };
+            mutableTaps = false;
+            autoMigrate = true;
+        };
     };
 
-  createDarwinSystemModules = host: username: fullname: thesystem: rec {
-    modules = [            
-        (./hosts/${host}.nix)
-        (./hardware/hardware-${host}.nix)            
-        (allowUnfree)
-        home-manager.nixosModules.home-manager {
-            home-manager.useUserPackages = true;
-            home-manager.useGlobalPkgs = true;
-            home-manager.users.${username} = (./users/${username}-${host}.nix);
-            home-manager.extraSpecialArgs = {                
-            pkgs-stable = import nixpkgs-stable  {
-                system = thesystem;
-                config.allowUnfree = true;
-            };             
+      createSystem = { helpers, host, username ? "howardsp", fullname ? "Howard Spector", thesystem ? "x86_64-linux"  }: nixpkgs.lib.nixosSystem {
+        system = thesystem;        
+        modules = [            
+            (./hosts/${host}.nix)
+            (./hardware/hardware-${host}.nix)            
+            (helpers.allowUnfree)
+            home-manager.nixosModules.home-manager {}
+            (helpers.homeHelper {username = username; fullname = fullname; host = host; thesystem = thesystem;})
+          ]; 
+          specialArgs = { 
+              pkgs-stable = import nixpkgs-stable  {
+                  system = thesystem;
+                  config.allowUnfree = true;
+              };             
+              inherit  host username fullname  home-manager thesystem;};
+        }; 
 
-            inherit host username fullname thesystem;
-            };     
-        } 
-        ]; 
-        specialArgs = { 
+      createSystemDarwin = { helpers, host, username ? "howardsp", fullname ? "Howard Spector", thesystem ? "aarch64-darwin"  }: darwin.lib.darwinSystem {
+          system = thesystem;          
+          modules = [
+            (./hosts/${host}.nix)
+            (helpers.allowUnfree)
+            home-manager.darwinModules.home-manager {}
+            (helpers.homeHelper {username = username; fullname = fullname; host = host; thesystem = thesystem;})
+            (helpers.homeBrewHelper {username = username;})             
+          ];
+          specialArgs = { 
             pkgs-stable = import nixpkgs-stable  {
-                system = thesystem;
-                config.allowUnfree = true;
+              system = thesystem;
+              config.allowUnfree = true;
             };             
-            inherit  host username fullname  home-manager thesystem;};
-    };
-in          
-    createLinuxSystemModules createDarwinSystemModules
+          inherit  host username fullname  inputs thesystem; };
+        };
 
-     
+}
