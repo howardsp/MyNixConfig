4fd9a0c24381852ae3362362cab05777f18fc6bb
Howard Spector <howardsp@gmail.com>
2024-12-31 11:12:51 -0500

    reorg

--- hosts/nixbookair.nix
+++ hosts/nixbookair.nix
@@ -3,44 +3,6 @@
 
   imports = [ ../system ];
  
-  homebrew = {
-    enable = true;      
-
-    onActivation = {
-      autoUpdate = true;
-      cleanup = "uninstall";
-      upgrade = true;
-    };
-
-    brews = [ "openjdk" ];
-    casks = [        
-        "telegram"
-        "zoom"
-        "citrix-workspace"
-        "google-chrome"
-        "firefox"
-        "microsoft-office"
-        "microsoft-edge"
-        "visual-studio-code"
-        "dropbox"
-        "unnaturalscrollwheels"        
-        "bettertouchtool"        
-        "aldente"
-        "insync"
-        "topnotch"
-        "karabiner-elements"
-        "notunes"
-        "webex"
-        "emacs"
-        "monitorcontrol"
-    ];
-
-    # mas = mac app store https://github.com/mas-cli/mas 
-    masApps = {        
-        "newsify" = 510153374;            
-        "amphetamine" = 937984704;
-    };
-  };    
             
   system.defaults = {
       #https://daiderd.com/nix-darwin/manual/index.html
