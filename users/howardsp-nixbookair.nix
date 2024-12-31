58c39da906119d13047807720e0e92fc04616405
Howard Spector <howardsp@gmail.com>
2024-12-30 20:36:37 -0500

    fix mac

--- users/howardsp-nixbookair.nix
+++ users/howardsp-nixbookair.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, username,   ... }:
 
 {     
-    imports = [ 
-        ./shell
-    ];
+    #imports = [ 
+    #    ./shell
+    #];
 
     home.stateVersion = "24.11";     
     programs.home-manager.enable = true;
