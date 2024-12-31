103434f379d4a8ba49b057202ca18de0acc93045
Howard Spector <howardsp@gmail.com>
2024-12-31 10:14:47 -0500

    mac fix

--- hosts/nixbookair.nix
+++ hosts/nixbookair.nix
@@ -1,47 +1,42 @@
 { config, lib, pkgs, host,username, fullname, ... }:
 {
  imports = [ ../system ];
+ 
+  homebrew = {
+    enable = true;      
+    onActivation = {
+      autoUpdate = true;
+      cleanup = "uninstall";
+      upgrade = true;
 
-  config = {
+      brews = [ "openjdk" ];
+      casks = [        
+          "telegram"
+          "zoom"
+          "citrix-workspace"
+          "google-chrome"
+          "firefox"
+          "microsoft-office"
+          "microsoft-edge"
+          "visual-studio-code"
+          "dropbox"
+          "unnaturalscrollwheels"        
+          "bettertouchtool"        
+          "aldente"
+          "insync"
+          "topnotch"
+          "karabiner-elements"
+          "notunes"
+          "webex"
+          "emacs"
+          "monitorcontrol"
+      ];
 
-   homebrew = {
-      enable = true;      
-      onActivation = {
-        autoUpdate = true;
-        cleanup = "uninstall";
-        upgrade = true;
-
-        brews = [ "openjdk" ];
-        casks = [        
-            "telegram"
-            "zoom"
-            "citrix-workspace"
-            "google-chrome"
-            "firefox"
-            "microsoft-office"
-            "microsoft-edge"
-            "visual-studio-code"
-            "dropbox"
-            "unnaturalscrollwheels"        
-            "bettertouchtool"        
-            "aldente"
-            "insync"
-            "topnotch"
-            "karabiner-elements"
-            "notunes"
-            "webex"
-            "emacs"
-            "monitorcontrol"
-        ];
-
-        # mas = mac app store https://github.com/mas-cli/mas 
-        masApps = {        
-            "newsify" = 510153374;            
-            "amphetamine" = 937984704;
-        };
+      # mas = mac app store https://github.com/mas-cli/mas 
+      masApps = {        
+          "newsify" = 510153374;            
+          "amphetamine" = 937984704;
+      };
     };            
-
-    };
-
   };
 }
