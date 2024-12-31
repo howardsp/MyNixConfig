317faf49462729ec12fb1c46bd274aee36363054
Howard Spector <howardsp@gmail.com>
2024-12-31 10:19:59 -0500

    mac fix

--- hosts/nixbookair.nix
+++ hosts/nixbookair.nix
@@ -8,38 +8,37 @@
       autoUpdate = true;
       cleanup = "uninstall";
       upgrade = true;
-
-      brews = [ "openjdk" ];
-      casks = [        
-          "telegram"
-          "zoom"
-          "citrix-workspace"
-          "google-chrome"
-          "firefox"
-          "microsoft-office"
-          "microsoft-edge"
-          "visual-studio-code"
-          "dropbox"
-          "unnaturalscrollwheels"        
-          "bettertouchtool"        
-          "aldente"
-          "insync"
-          "topnotch"
-          "karabiner-elements"
-          "notunes"
-          "webex"
-          "emacs"
-          "monitorcontrol"
-      ];
-
-      # mas = mac app store https://github.com/mas-cli/mas 
-      masApps = {        
-          "newsify" = 510153374;            
-          "amphetamine" = 937984704;
       };
-    };            
-  };
-    system.defaults = {
+    brews = [ "openjdk" ];
+    casks = [        
+        "telegram"
+        "zoom"
+        "citrix-workspace"
+        "google-chrome"
+        "firefox"
+        "microsoft-office"
+        "microsoft-edge"
+        "visual-studio-code"
+        "dropbox"
+        "unnaturalscrollwheels"        
+        "bettertouchtool"        
+        "aldente"
+        "insync"
+        "topnotch"
+        "karabiner-elements"
+        "notunes"
+        "webex"
+        "emacs"
+        "monitorcontrol"
+    ];
+
+    # mas = mac app store https://github.com/mas-cli/mas 
+    masApps = {        
+        "newsify" = 510153374;            
+        "amphetamine" = 937984704;
+    };
+  };              
+  system.defaults = {
       #https://daiderd.com/nix-darwin/manual/index.html
       WindowManager.GloballyEnabled = true;
 
