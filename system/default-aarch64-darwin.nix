39a4e6286613b69929ec2b5ec9ba3ca6170f49fb
Howard Spector <howardsp@gmail.com>
2024-12-30 22:43:50 -0500

    test

--- system/default-aarch64-darwin.nix
+++ system/default-aarch64-darwin.nix
@@ -12,7 +12,7 @@
     environment.systemPackages = with pkgs; [ mas ];         
 
     homebrew = {
-        brews = [ "openjdk" ];
+        brews = [  ];
         casks = [        
             "telegram"
             "zoom"
