8201f5296affd019d6b2d414dec530a5db3f88f5
Howard Spector <howardsp@gmail.com>
2025-03-09 15:08:47 -0400

    added virtual

--- system/default-aarch64-darwin.nix
+++ system/default-aarch64-darwin.nix
@@ -13,7 +13,7 @@
         home = "/Users/${username}";            
     };        
 
-    environment.systemPackages = with pkgs; [ mas ];         
+    environment.systemPackages = with pkgs; [ mas qemu_full ];
 
     homebrew = {
         enable = true;      
@@ -44,7 +44,7 @@
             "notunes"
             "webex"            
             "monitorcontrol"
-            "iterm2"
+            "iterm2"            
         ];
 
         # mas = mac app store https://github.com/mas-cli/mas 
