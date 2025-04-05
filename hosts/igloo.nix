cc6622af365942c15939e4b787cefd5b3bedb51f
Howard Spector <howardsp@gmail.com>
2025-04-05 07:54:19 -0400

    upgrade to kernel 14

--- hosts/igloo.nix
+++ hosts/igloo.nix
@@ -5,9 +5,9 @@
   __qemu.enable = true;
   __synergy-server.enable = true;  
 
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_11;
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_6_14;
   boot.kernelModules = [ "kvm-amd" "cpufreq_performance" "v4l2loopback" ];    
-  boot.extraModulePackages = [ pkgs.linuxKernel.packages.linux_6_11.v4l2loopback ];
+  boot.extraModulePackages = [ pkgs.linuxKernel.packages.linux_6_14.v4l2loopback ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;  
   
