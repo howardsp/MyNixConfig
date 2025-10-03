3df1b3ec783481869d91b124d72ddc2472ac1b55
Howard Spector <howardsp@gmail.com>
2025-10-02 20:54:27 -0400

    home ass

--- system/linux/containers/homeassistant.nix
+++ system/linux/containers/homeassistant.nix
@@ -2,19 +2,21 @@
 {
       config = lib.mkIf (config.myHomeAssistant.enable)
       {       
-        virtualisation.oci-containers.containers."homeassistant" = {
-            autoStart = true;
-            image = "ghcr.io/home-assistant/home-assistant:stable";
-            volumes = [
-            "/home/${username}/HomeAssistant:/config"
-            "/etc/localtime:/etc/localtime:ro"
-            ];
-            extraOptions = [
-            "--device=/dev/ttyUSB0"
-            "--network=host"
-            "--privileged"
-            ];
-        };        
+          virtualisation.oci-containers = {
+            backend = "podman";
+            containers.homeassistant = {
+              volumes = [ "home-assistant:/config" ];
+              environment.TZ = "America/New_York";
+              # Note: The image will not be updated on rebuilds, unless the version label changes
+              image = "ghcr.io/home-assistant/home-assistant:stable";
+              extraOptions = [ 
+                # Use the host network namespace for all sockets
+                "--network=host"
+                # Pass devices into the container, so Home Assistant can discover and make use of them
+                "--device=/dev/ttyACM0:/dev/ttyACM0"
+              ];
+            };
+          };
 
         networking.firewall.allowedTCPPortRanges = [ { from = 8123; to = 8123; } ];
 
